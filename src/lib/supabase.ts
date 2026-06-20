d373df01135cc94078210c2c47b70a7f124344dc
Lawrence Saludes <lawrencejay.saludes@hcdc.edu.ph>
2026-06-20 21:58:06 +0800

    supabase env fixed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,10 @@
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import Constants from "expo-constants";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl as string;
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseAnonKey as string;
 
 export const supabase = createClient(
   supabaseUrl,
